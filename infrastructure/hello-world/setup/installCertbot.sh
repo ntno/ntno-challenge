23cdb7b15989593c57c14b23be41a84e24bce0d6
ntno <organick.natan@gmail.com>
2019-08-30 16:29:58 -0400

    started using config from generator, also trying out certbot

---
+++ infrastructure/hello-world/setup/installCertbot.sh
@@ -0,0 +1,5 @@
+#https://certbot.eff.org/lets-encrypt/pip-nginx
+wget https://dl.eff.org/certbot-auto
+mv certbot-auto /usr/local/bin/certbot-auto
+chown root /usr/local/bin/certbot-auto
+chmod 0755 /usr/local/bin/certbot-auto
