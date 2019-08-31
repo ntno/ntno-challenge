1e2987701e8a733647d1047ed29138e855b60235
ntno <organick.natan@gmail.com>
2019-08-31 12:52:08 -0400

    added ecr repo for docker images

--- infrastructure/update-stack.sh
+++ infrastructure/update-stack.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-#create-stack.sh
+#update-stack.sh
 templateName=pipeline.yml
 stackName=ntno-challenge-pipeline
 
 repoName=hello-world
-repoDescription=serves static site securely
+repoDescription="serves static site securely"
 
 aws s3 cp pipeline.yml s3://ntno-misc/cft/$templateName --sse
-aws cloudformation create-stack --template-url https://ntno-misc.s3.amazonaws.com/cft/$templateName \
+aws cloudformation update-stack --template-url https://ntno-misc.s3.amazonaws.com/cft/$templateName \
                     --capabilities CAPABILITY_NAMED_IAM \
                     --stack-name $stackName \
                     --parameters  ParameterKey=RepoName,ParameterValue=$repoName \
-                                  ParameterKey=RepoDescription,ParameterValue=$repoDescription
+                                  ParameterKey=RepoDescription,ParameterValue="$repoDescription"
