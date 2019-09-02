bdf30140d9e9d396b44f2bbf9cc7a2ff9f92c1a1
ntno <organick.natan@gmail.com>
2019-09-02 15:19:01 -0400

    some more log refactoring

--- code/validate.py
+++ code/validate.py
@@ -4,7 +4,7 @@ from bracelogs import BraceMessage as __
 
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',)
 LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
+# LOGGER.setLevel(logging.DEBUG)
 
 matchCardFormat = re.compile(r"^[456][0-9]{3}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}$")
 matchDuplicateCharacters = re.compile(r"(.)\1{3,}")
@@ -20,20 +20,21 @@ def readLinesFromStdin():
 
 def validateLine(line):
     isValid = False
-    debugMessage = ""
     matchOnCardFormat = re.fullmatch(matchCardFormat, line)
     if(matchOnCardFormat):
         matchOnDuplicates = re.findall(matchDuplicateCharacters, line.replace("-", ""))
         if(matchOnDuplicates):
             isValid = False
-            debugMessage = "at least one occurence of 4 consecutive repeated characters"
+            LOGGER.debug(__("{line},{validity},{details}", line=line, validity=isValid, details="at least one occurence of 4 consecutive repeated characters")) 
         else:
             isValid = True
-            debugMessage = "no 4 consecutive repeated characters"
+            LOGGER.debug(__("{line},{validity},{details}", line=line, validity=isValid, details="no 4 consecutive repeated characters")) 
+
     else:
         isValid = False
-        debugMessage = "bad card format"
-    return (line, isValid, debugMessage)
+        LOGGER.debug(__("{line},{validity},{details}", line=line, validity=isValid, details="bad card format")) 
+    return (line, isValid)
+
 
 def validateLines(lines):
     answers = []
@@ -41,13 +42,16 @@ def validateLines(lines):
         answers.append(validateLine(l))
     return answers
 
-
-if __name__ == "__main__":
+def main():
     lines = readLinesFromStdin()
     answers = validateLines(lines)
     for answer in answers:
-        LOGGER.debug(__("{line},{details}", line=answer[0], details=answer[2]))
         if(answer[1]):
             print ("Valid")
         else: 
             print ("Invalid")
+
+
+if __name__ == "__main__":
+    main()
+    
