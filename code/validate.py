ff132a319313d33539b5868e8c22d179af815774
ntno <organick.natan@gmail.com>
2019-09-02 14:52:50 -0400

    refactoring

--- code/validate.py
+++ code/validate.py
@@ -2,41 +2,46 @@
 import fileinput, re
 DEBUG = False
 
-
-##############
-# define regex
-##############
 matchCardFormat = re.compile(r"^[456][0-9]{3}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}$")
 matchDuplicateCharacters = re.compile(r"(.)\1{3,}")
 
-###############
-# read in lines
-###############
-lines = []
-for line in fileinput.input():
-    lines.append(line.rstrip())
+def readLinesFromStdin():
+    lines = []
+    for line in fileinput.input():
+        lines.append(line.rstrip())
 
-#remove the line count
-lines = lines[1:]   
+    #remove the line count
+    lines = lines[1:]   
+    return lines
 
-################
-# validate lines
-################
-for l in lines:
+def validateLine(line):
     message = ""
-    matchOnCardFormat = re.fullmatch(matchCardFormat, l)
+    matchOnCardFormat = re.fullmatch(matchCardFormat, line)
     if(matchOnCardFormat):
-        matchOnDuplicates = re.findall(matchDuplicateCharacters, l.replace("-", ""))
+        matchOnDuplicates = re.findall(matchDuplicateCharacters, line.replace("-", ""))
         if(matchOnDuplicates):
             message = "Invalid"
             if(DEBUG):
-                message = "{},{},{}".format(l, message,  "at least one occurence of 4 consecutive repeated characters")
+                message = "{},{},{}".format(line, message,  "at least one occurence of 4 consecutive repeated characters")
         else:
-             message = "Valid" 
-             if(DEBUG):
-                message = "{},{},{}".format(l, message,  "no 4 consecutive repeated characters")
+            message = "Valid" 
+            if(DEBUG):
+                message = "{},{},{}".format(line, message,  "no 4 consecutive repeated characters")
     else:
         message = "Invalid" 
         if(DEBUG):
-            message = "{},{},{}".format(l, message,  "bad card format")
-    print (message)
+            message = "{},{},{}".format(line, message,  "bad card format")
+    return message
+
+def validateLines(lines):
+    answers = []
+    for l in lines:
+        answers.append(validateLine(l))
+    return answers
+
+
+if __name__ == "__main__":
+    lines = readLinesFromStdin()
+    answers = validateLines(lines)
+    for answ in answers:
+        print (answ)
