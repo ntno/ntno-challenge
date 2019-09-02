9615f2cbe6de3fc9ac683447c456c656b938828f
ntno <organick.natan@gmail.com>
2019-09-02 14:03:57 -0400

    initial program

--- code/validate.py
+++ code/validate.py
@@ -1,5 +1,48 @@
-#TODO - repeating numbers
-^[456][0-9]{3}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}$
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 
-#matches repeating numbers but need to count groups - could count manually
-(.)\1
+import fileinput, re
+DEBUG = True
+
+
+##############
+# define regex
+##############
+matchCardFormat = re.compile(r"^[456][0-9]{3}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}$")
+matchDuplicateCharacters = re.compile(r"(.)\1")
+
+###############
+# read in lines
+###############
+lines = []
+for line in fileinput.input():
+    lines.append(line.rstrip())
+
+#remove the line count
+lines = lines[1:]   
+
+################
+# validate lines
+################
+for l in lines:
+    message = ""
+    matchOnCardFormat = re.fullmatch(matchCardFormat, l)
+    if(matchOnCardFormat):
+        matchOnDuplicates = re.findall(matchDuplicateCharacters, l)
+        if(matchOnDuplicates):
+            if(len(matchOnDuplicates) > 1):
+                 message = "Invalid"
+                 if(DEBUG):
+                     message = message + " - more than one duplicate character"
+            else:
+                 message = "Valid" 
+                 if(DEBUG):
+                     message = message + " - only one duplicate character" 
+        else:
+             message = "Valid" 
+             if(DEBUG):
+                 message = message + " - no duplicate characters"
+    else:
+        message = "Invalid" 
+        if(DEBUG):
+            message = message + " - bad card format"
+    print (message)
