cc1710281e73c0f3ea185b373a108922ba929c32
ntno <organick.natan@gmail.com>
2019-09-04 17:08:37 -0400

    small refactor, added todos

--- infrastructure/update-app-deploy-stack.sh
+++ infrastructure/update-app-deploy-stack.sh
@@ -9,5 +9,5 @@ aws s3 cp $templateName s3://ntno-misc/cft/$templateName --sse
 aws cloudformation update-stack --template-url https://ntno-misc.s3.amazonaws.com/cft/$templateName \
                     --capabilities CAPABILITY_NAMED_IAM \
                     --stack-name $stackName \
-                    --parameters  ParameterKey=SSHKeyName,ParameterValue=$sshKeyPair \
-                    --parameters  ParameterKey=LogGroupName,ParameterValue=ntno-challenge-docker-logs
+                    --parameters  ParameterKey=LogGroupName,ParameterValue=ntno-challenge-docker-logs \
+                    --parameters  ParameterKey=SSHKeyName,ParameterValue="$sshKeyPair" 
