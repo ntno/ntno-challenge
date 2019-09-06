893acb868dc6e075094631c55aaca033ca05bd77
ntno <organick.natan@gmail.com>
2019-09-06 15:33:07 -0400

    refactoring helper scripts

--- infrastructure/cloudformation/update-artifact-bucket-stack.sh
+++ infrastructure/cloudformation/update-artifact-bucket-stack.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
-Region=$(aws configure get region)
-BucketName="ntno-code-pipeline-artifacts-${Region}"
-StackName=$BucketName
-templateName="cft/artifact-bucket.yml"
+# #!/bin/sh
+# Region=$(aws configure get region)
+# BucketName="ntno-code-pipeline-artifacts-${Region}"
+# StackName=$BucketName
+# templateName="cft/artifact-bucket.yml"
 
-aws s3 cp $templateName s3://ntno-misc/$templateName --sse
-aws cloudformation validate-template --template-body file://$templateName
-aws cloudformation update-stack --template-url https://ntno-misc.s3.amazonaws.com/$templateName \
-                                --stack-name ${StackName}  \
-                                --parameters  ParameterKey=CustomBucketName,ParameterValue=${BucketName} \
-                                              ParameterKey=DomainTag,ParameterValue=personal 
+# aws s3 cp $templateName s3://ntno-misc/$templateName --sse
+# aws cloudformation validate-template --template-body file://$templateName
+# aws cloudformation update-stack --template-url https://ntno-misc.s3.amazonaws.com/$templateName \
+#                                 --stack-name ${StackName}  \
+#                                 --parameters  ParameterKey=CustomBucketName,ParameterValue=${BucketName} \
+#                                               ParameterKey=DomainTag,ParameterValue=personal 
 
-export NTNO_CHALLENGE_ARTIFACT_BUCKET_NAME=$BucketName
+# export NTNO_CHALLENGE_ARTIFACT_BUCKET_NAME=$BucketName
--- infrastructure/cloudformation/update-pipeline-stack.sh
+++ infrastructure/cloudformation/update-pipeline-stack.sh
@@ -1,19 +1,17 @@
-#!/bin/sh
-#update-stack.sh
-templateName="cft/pipeline.yml"
-stackName="ntno-challenge-pipeline"
+# #!/bin/sh
+# #update-stack.sh
+# templateName="cft/pipeline.yml"
+# stackName="ntno-challenge-pipeline"
 
-repoName="hello-world"
-repoDescription="serves static site securely"
-artifactBucketName=$NTNO_CHALLENGE_ARTIFACT_BUCKET_NAME        #see create-artifact-bucket-stack.sh
+# repoName="hello-world"
+# repoDescription="serves static site securely"
 
-aws s3 cp $templateName s3://ntno-misc/$templateName --sse
-aws cloudformation validate-template --template-body file://$templateName
-aws cloudformation update-stack --template-url https://ntno-misc.s3.amazonaws.com/$templateName \
-                    --capabilities CAPABILITY_NAMED_IAM \
-                    --stack-name $stackName \
-                    --parameters  ParameterKey=RepoName,ParameterValue=$repoName \
-                                  ParameterKey=RepoDescription,ParameterValue="$repoDescription" \
-                                  ParameterKey=ArtifactBucketName,ParameterValue=$artifactBucketName \
-                                  ParameterKey=DomainTag,ParameterValue=personal \
-                                  ParameterKey=ProjectTag,ParameterValue=ntno-challenge \
+# aws s3 cp $templateName s3://ntno-misc/$templateName --sse
+# aws cloudformation validate-template --template-body file://$templateName
+# aws cloudformation update-stack --template-url https://ntno-misc.s3.amazonaws.com/$templateName \
+#                     --capabilities CAPABILITY_NAMED_IAM \
+#                     --stack-name $stackName \
+#                     --parameters  ParameterKey=RepoName,ParameterValue=$repoName \
+#                                   ParameterKey=RepoDescription,ParameterValue="$repoDescription" \
+#                                   ParameterKey=DomainTag,ParameterValue=personal \
+#                                   ParameterKey=ProjectTag,ParameterValue=ntno-challenge \
